39b64247722a8bad7f2c0e6b1a1c659a280bde66
tapadar13 <monsurismyname@gmail.com>
2023-06-03 22:13:35 +0530

    set up post route

--- routes/users.route.js
+++ routes/users.route.js
@@ -1,10 +1,37 @@
 import express from "express";
 
+import { v4 as uuidv4 } from "uuid";
+uuidv4(); // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
+
 const router = express.Router();
 
+const users = [
+  {
+    firstName: "John",
+    lastName: "Doe",
+    age: 30,
+  },
+  {
+    firstName: "Jane",
+    lastName: "Doe",
+    age: 29,
+  },
+];
+
 router.get("/", (req, res) => {
-  console.log("Hi router");
-  res.send("Hello");
+  console.log(users);
+  res.send(users);
 });
+
+router.post("/", (req, res) => {
+  const user = req.body;
+  users.push(user);
+
+  res.send(`User with the name ${user.firstName} added to the database!`);
+});
+
+// router.get("/:id", (req, res) => {
+//   const { id } = req.params;
+// });
 
 export default router;
