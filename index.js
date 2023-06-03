ff8a5dbbad4fbdd235adf4736e8a3db5ecbcedbb
tapadar13 <monsurismyname@gmail.com>
2023-06-03 18:26:53 +0530

    set up a route

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 import express from "express";
 import bodyParser from "body-parser";
 
+import userRoutes from "./routes/users.route.js";
+
 const app = express();
 const PORT = 8000;
 
 // initializing the middleware
 app.use(bodyParser.json()); // tells that we are going to be using JSON data in our entire application
+
+app.use("/users", userRoutes);
+
+app.get("/", (req, res) => res.send("Hi from the homepage"));
 
 app.listen(PORT, () => {
   console.log(`Server is now successfully running on http://localhost:${PORT}`);
