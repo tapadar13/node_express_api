107fe23a85d9e3e90533994164df31b4819f19b5
tapadar13 <monsurismyname@gmail.com>
2023-06-04 08:51:09 +0530

    set up get specific user route

--- routes/users.route.js
+++ routes/users.route.js
@@ -1,22 +1,10 @@
 import express from "express";
 
 import { v4 as uuidv4 } from "uuid";
-uuidv4(); // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
 
 const router = express.Router();
 
-const users = [
-  {
-    firstName: "John",
-    lastName: "Doe",
-    age: 30,
-  },
-  {
-    firstName: "Jane",
-    lastName: "Doe",
-    age: 29,
-  },
-];
+let users = [];
 
 router.get("/", (req, res) => {
   console.log(users);
@@ -25,13 +13,18 @@ router.get("/", (req, res) => {
 
 router.post("/", (req, res) => {
   const user = req.body;
-  users.push(user);
+
+  users.push({ ...user, id: uuidv4() });
 
   res.send(`User with the name ${user.firstName} added to the database!`);
 });
 
-// router.get("/:id", (req, res) => {
-//   const { id } = req.params;
-// });
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+
+  const user = users.find((user) => user.id === id);
+
+  res.send(user);
+});
 
 export default router;
